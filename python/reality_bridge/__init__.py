d153ece725cf46603b435a6915a225e51307b6e3
NavisWORLD <personal.phera@gmail.com>
2026-08-13 10:52:45 -0500

    Export Python Synaptic Core API

--- python/reality_bridge/__init__.py
+++ python/reality_bridge/__init__.py
@@ -1,5 +1,7 @@
-"""Reality Bridge reusable musical-state engine."""
+"""Reality Bridge reusable musical-state and Synaptic Core engine."""
 from .engine import (AccompanimentEngine, BridgeState, HarmonicHypothesis,
                      MediaDNA, NoteEvent, VoiceDNA, midi_to_hz, hz_to_midi)
-__all__=['AccompanimentEngine','BridgeState','HarmonicHypothesis','MediaDNA','NoteEvent','VoiceDNA','midi_to_hz','hz_to_midi']
-__version__='0.1.0'
+from .synapse import (SYNAPSE_VERSION, Synapse, SynapseConfig, SynapseState,
+                      conformance_sequence)
+__all__=['AccompanimentEngine','BridgeState','HarmonicHypothesis','MediaDNA','NoteEvent','VoiceDNA','midi_to_hz','hz_to_midi','SYNAPSE_VERSION','Synapse','SynapseConfig','SynapseState','conformance_sequence']
+__version__='0.3.0'
